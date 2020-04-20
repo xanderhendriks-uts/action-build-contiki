3e0e00eca23b9afce63a4dbd322861d1f6b5cc02
Xander Hendriks <xander@nx-solutions.com>
2020-04-20 21:47:20 +1000

    Enable the build again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh -l
 
-#git submodule update --init --recursive
-#cd /github/workspace/examples/cc26xx/cc26xx-web-demo
-#make clean TARGET=srf06-cc26xx BOARD=sensortag/cc2650
-pwd
-ls -la
-ls -la /github/workspace
-ls -la /
-cat /entrypoint.sh
+cd examples/cc26xx/cc26xx-web-demo
+make clean TARGET=srf06-cc26xx BOARD=sensortag/cc2650
