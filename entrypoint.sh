7586495c13d96b9d76617a64500a4d08e7acb052
Xander Hendriks <xander@nx-solutions.com>
2020-04-20 16:58:33 +1000

    Update to the correct workarea path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 git submodule update --init --recursive
-cd examples/cc26xx/cc26xx-web-demo
+cd /github/workspace/examples/cc26xx/cc26xx-web-demo
 make clean TARGET=srf06-cc26xx BOARD=sensortag/cc2650
