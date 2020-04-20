74e6e7753265acc52a4d9b7f22137e581809c3cc
Xander Hendriks <xander@nx-solutions.com>
2020-04-20 17:12:13 +1000

    Some more experiments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@
 #git submodule update --init --recursive
 #cd /github/workspace/examples/cc26xx/cc26xx-web-demo
 #make clean TARGET=srf06-cc26xx BOARD=sensortag/cc2650
+pwd
 ls -la
+ls -la /github/workspace
+ls -la /
+cat /entrypoint.sh
