b02615251d2b5a23656fdc723faf8be07d07b4b1
Xander Hendriks <xander@nx-solutions.com>
2020-05-16 15:55:44 +1000

    Added parameters for build selection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh -l
 
-cd examples/cc26xx/cc26xx-web-demo
-sudo make TARGET=srf06-cc26xx BOARD=sensortag/cc2650
+if [ $1 == "cc26xx-web-demo" ]
+then
+	cd examples/cc26xx/cc26xx-web-demo
+	sudo make TARGET=srf06-cc26xx BOARD=$2/cc2650
+elif [ $1 == "slip-radio" ]
+then
+	cd examples/ipv6/slip-radio
+	sudo make TARGET=srf06-cc26xx BOARD=$2/cc2650
+else
+	exit 1
+fi
